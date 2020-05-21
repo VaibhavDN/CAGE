44e707289a1260169fa4074ed08f2c5a07d366c3
Vaibhav Dwivedi <44027469+VaibhavDN@users.noreply.github.com>
2020-05-21 17:56:02 +0530

    Minor usability changes

--- usersCode2.cpp
+++ usersCode2.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void fun_call(){
+    for(int i=0; i<5; i++){
+        int m=i;
+        m+=m;
+    }
+}
 
-int main(){ //depth 1 level 0
+int main(){
     int n = 5;
-    if(n>2){    //depth 2 level 1
-        if(n<10){   //depth 3 level 2
-            for(int j=0; j<n; j++){ //depth 4 level 3
-                //pass
-            }
-        }
-        else{   //depth 3 level 2
-            for(int k=0; k<2*n; k++){   //depth 4 level 3
-                if(k == 5){ //depth 5 level 4
-                    //pass
+    while(n--){
+        if(n>2){
+            if(n<10){
+                for(int j=0; j<n; j++){
+                    int m=j;
+                    m=m%3;
                 }
             }
+            else{
+                for(int k=0; k<2*n; k++){
+                    if(k == 5){
+                        int m=k;
+                        m*=m;
+                    }
+                }
+            }
+        }
+        else{
+            fun_call();
         }
     }
     return 0;
 }
-
-/*
-4 6 0
-12 14 3
-11 15 2
-18 20 4
-17 21 3
-16 22 2
-10 23 1
-8 25 0
-*/
